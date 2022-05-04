84b3221ae31b9019b7e97a62d4079a4796d9ce20
Net Chenda <netchenda@macbook-pro-de-net.home>
2022-05-04 13:24:02 +0200

    install react router dom and created routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Product from "./components/pages/Product";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Bienvenur sur l'accueil</h1>
+        </div>
+    )
+}
