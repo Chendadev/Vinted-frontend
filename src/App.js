5449c9b90b9aae457f61c314b75f2911da0e59b5
Net Chenda <netchenda@mbp-de-net.lan>
2022-05-04 15:01:41 +0200

    connected to api data vinted backend

--- src/App.js
+++ src/App.js
@@ -7,30 +7,46 @@ import NotFound from "./components/containers/NotFound";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://lereacteur-vinted-api.herokuapp.com/offers");
+        // console.log("voir data", response)
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [])
+
+  return isLoading === true ? (<div>En cours de chargement ...</div>) :
+    (
+      <div className="App">
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </div>
+    );
 }
 export default App;
