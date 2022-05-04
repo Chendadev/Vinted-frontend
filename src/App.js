dccacaebb797bc949e4ebbaefb17b908727727b7
Net Chenda <netchenda@mbp-de-net.lan>
2022-05-04 16:12:30 +0200

    correction component Product to Offer component and add method map to find image + quality + brand

--- src/App.js
+++ src/App.js
@@ -3,50 +3,34 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/containers/Home";
 import Product from "./components/containers/Product";
 import About from "./components/containers/About";
+import Offer from "./components/containers/Offer";
 import NotFound from "./components/containers/NotFound";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 function App() {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://lereacteur-vinted-api.herokuapp.com/offers");
-        // console.log("voir data", response)
-        setData(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [])
-
-  return isLoading === true ? (<div>En cours de chargement ...</div>) :
-    (
-      <div className="App">
-        <Router>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/offers">Offers</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/offers" element={<Offer />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 export default App;
--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,10 +1,54 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Bienvenue sur l'accueil</h1>
+    const [data, setData] = useState();
+    const [isLoading, setIsLoading] = useState(true);
 
-        </div>
-    )
-}
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("https://lereacteur-vinted-api.herokuapp.com/offers");
+                // console.log("voir data", response)
+                setData(response.data);
+                setIsLoading(false);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, [])
+
+    return isLoading === true ? (<div>En cours de chargement ...</div>) :
+        (
+            <div className="home">
+                <h1>Bienvenue sur l'accueil</h1>
+                {data.offers.map((offer, index) => {
+                    console.log("yakoila", offer.product_details)
+                    return (
+                        <div className="card">
+                            {offer.product_pictures.map((product, num) => {
+                                return (
+                                    <div>
+                                        <img src={product.secure_url} />
+                                        <p>{product.product_price}</p>
+                                    </div>
+
+                                )
+                            })}
+                            {offer.product_details.map((item, num) => {
+                                return (
+                                    <div>
+                                        <p>{item.ÉTAT}</p>
+                                        <p>{item.MARQUE}</p>
+                                    </div>
+
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+            </div>
+        )
+};
